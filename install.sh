ba07b9bc1f9d4de2e16ee9c8af27f715a0978c91
fknoahh <2509662915@qq.com>
2024-08-22 17:12:15 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/data61/MP-SPDZ.git &&
 cp -r MP-SPDZ/* . &&
-rm -r MP-SPDZ &&
+sudo rm -r MP-SPDZ &&
 sudo apt-get install -y automake build-essential clang cmake git libboost-dev libboost-iostreams-dev libboost-thread-dev libgmp-dev libntl-dev libsodium-dev libssl-dev libtool python3 &&
 make setup &&
 mkdir Player-Data &&
