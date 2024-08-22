2855508c6fa1fbfc2b80a8fb50d8baecf0908cd4
fknoahh <2509662915@qq.com>
2024-08-22 18:44:24 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+
+conda create -y -n sampling-py python=3.9
+conda activate sampling-py
+conda install -y matplotlib==3.6.1 mpmath==1.3.0 numpy==1.22.4 scipy==1.7.1 pandas
+
 git clone https://github.com/data61/MP-SPDZ.git &&
 cp -r MP-SPDZ/* . &&
 rm -r MP-SPDZ &&
 apt update
 apt-get install -y automake build-essential clang cmake git libboost-dev libboost-iostreams-dev libboost-thread-dev libgmp-dev libntl-dev libsodium-dev libssl-dev libtool python3 &&
 make setup &&
-mkdir Player-Data &&
-pip install -r requirements.txt &&
 bash Scripts/setup-ssl.sh 16 &&
 make -j8 shamir-bmr-party.x &&
 make -j8 yao-party.x
