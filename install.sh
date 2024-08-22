a1a76b1db4e721fa0dea2ff02bf69ca9f7ceca97
fknoahh <2509662915@qq.com>
2024-08-22 16:54:03 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ sudo apt-get install -y automake build-essential clang cmake git libboost-dev li
 make setup &&
 mkdir Player-Data &&
 pip install -r requirements.txt &&
-python random_bit.py 8 &&
 bash Scripts/setup-ssl.sh 8 &&
 make -j8 shamir-bmr-party.x &&
 make -j8 yao-party.x
