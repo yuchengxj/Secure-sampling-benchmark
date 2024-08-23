fe6f501961f4749f15ea7c96f6faf0cba146de44
fknoahh <2509662915@qq.com>
2024-08-22 21:11:39 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-conda env remove -n sampling-py &&
-conda create -y -n sampling-py python=3.9 &&
-conda activate sampling-py &&
-conda install -y matplotlib==3.6.1 mpmath==1.3.0 numpy==1.22.4 scipy==1.7.1 pandas &&
+conda env remove -n sampling-py 
+conda create -y -n sampling-py python=3.9 
+conda install -n sampling-py   matplotlib mpmath numpy scipy pandas 
+
 git clone https://github.com/data61/MP-SPDZ.git &&
 cp -r MP-SPDZ/* . &&
 sudo rm -r MP-SPDZ &&
@@ -12,4 +12,4 @@ sudo apt-get install -y automake build-essential clang cmake git libboost-dev li
 sudo make setup &&
 sudo bash Scripts/setup-ssl.sh 16 &&
 make -j8 shamir-bmr-party.x &&
-make -j8 yao-party.x
+make -j8 yao-party.x &&
