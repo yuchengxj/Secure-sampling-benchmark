576b63f4318249278f45522783d0eef516d2efa0
fknoahh <2509662915@qq.com>
2024-08-22 23:24:02 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ conda install -n sampling-py   matplotlib mpmath numpy scipy pandas
 
 git clone https://github.com/data61/MP-SPDZ.git &&
 cp -r MP-SPDZ/* . &&
-sudo rm -r MP-SPDZ &&
-sudo apt update
+rm -r MP-SPDZ &&
+sudo apt update &&
 sudo apt-get install -y automake build-essential clang cmake git libboost-dev libboost-all-dev libboost-iostreams-dev libboost-thread-dev libgmp-dev libntl-dev libsodium-dev libssl-dev libtool python3 &&
 sudo make setup &&
 sudo bash Scripts/setup-ssl.sh 16 &&
